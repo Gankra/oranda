77ffbb80b77081007b8fd1ac7cf4dd7da675b8b8
Aria Beingessner <a.beingessner@gmail.com>
2023-05-23 18:39:29 -0400

    make a ton of progress on axomdbook

--- src/site/mdbook.rs
+++ src/site/mdbook.rs
@@ -9,6 +9,8 @@ use crate::site::Site;
 
 use toml_edit::value;
 
+use super::markdown::SyntaxTheme;
+
 /// Name of the config for mdbook
 const MDBOOK_TOML: &str = "book.toml";
 
@@ -17,6 +19,8 @@ const THEME_GENERAL_CSS_PATH: &str = "css/general.css";
 const THEME_GENERAL_CSS: &str = include_str!("../../oranda-css/mdbook-theme/css/general.css");
 const THEME_VARIABLES_CSS_PATH: &str = "css/variables.css";
 const THEME_VARIABLES_CSS: &str = include_str!("../../oranda-css/mdbook-theme/css/variables.css");
+const THEME_CHROME_CSS_PATH: &str = "css/chrome.css";
+const THEME_CHROME_CSS: &str = include_str!("../../oranda-css/mdbook-theme/css/chrome.css");
 const THEME_FONTS_CSS_PATH: &str = "fonts/fonts.css";
 const THEME_FONTS_CSS: &str = include_str!("../../oranda-css/mdbook-theme/fonts/fonts.css");
 const THEME_BOOK_JS_PATH: &str = "book.js";
@@ -24,8 +28,11 @@ const THEME_BOOK_JS: &str = include_str!("../../oranda-css/mdbook-theme/book.js"
 const THEME_INDEX_HBS_PATH: &str = "index.hbs";
 const THEME_INDEX_HBS: &str = include_str!("../../oranda-css/mdbook-theme/index.hbs");
 
+const THEME_HIGHLIGHT_CSS_PATH: &str = "highlight.css";
+const THEMES: &[(&str, &str)] = &[("MaterialTheme", include_str!("../../oranda-css/mdbook-theme/highlight-js-themes/base16-material.css"))];
+
 /// Build and write mdbook to the dist dir
-pub fn build_mdbook(dist: &Utf8Path, book_cfg: &MdBookConfig) -> Result<()> {
+pub fn build_mdbook(dist: &Utf8Path, book_cfg: &MdBookConfig, syntax_theme: &SyntaxTheme) -> Result<()> {
     Message::new(MessageType::Info, "Building mdbook...").print();
     tracing::info!("Building mdbook...");
 
@@ -36,7 +43,7 @@ pub fn build_mdbook(dist: &Utf8Path, book_cfg: &MdBookConfig) -> Result<()> {
         let md = load_mdbook(&book_dir)?;
         let theme_dir =
             Utf8PathBuf::from_path_buf(md.theme_dir()).expect("mdbook theme path wasn't utf8");
-        init_theme(&book_dir, &theme_dir)?;
+        init_theme(&book_dir, &theme_dir, syntax_theme)?;
     }
 
     let md = load_mdbook(&book_dir)?;
@@ -67,27 +74,38 @@ fn load_mdbook(book_dir: &Utf8Path) -> Result<MDBook> {
     Ok(md)
 }
 
-fn init_theme(book_dir: &Utf8Path, theme_dir: &Utf8Path) -> Result<()> {
+fn init_theme(book_dir: &Utf8Path, theme_dir: &Utf8Path, syntax_theme: &SyntaxTheme) -> Result<()> {
     Message::new(MessageType::Info, "Adding oranda mdbook theme...").print();
     tracing::info!("Adding oranda mdbook theme...");
 
-    init_theme_files(theme_dir)?;
+    init_theme_files(theme_dir, syntax_theme)?;
     add_theme_to_book_toml(book_dir)?;
 
     Ok(())
 }
 
-fn init_theme_files(theme_dir: &Utf8Path) -> Result<()> {
+fn init_theme_files(theme_dir: &Utf8Path, syntax_theme: &SyntaxTheme) -> Result<()> {
     if theme_dir.exists() {
         LocalAsset::remove_dir_all(theme_dir.as_str())?;
     }
 
+    let theme_name = syntax_theme.as_str();
+    let highlight_theme = THEMES.iter().find_map(|(name, contents)| {
+        if *name == theme_name {
+            Some(*contents)
+        } else {
+            None
+        }
+    }).expect("failed to find highlightjs syntax theme for mdbook!?");
+
     let files = vec![
         (THEME_GENERAL_CSS_PATH, THEME_GENERAL_CSS),
         (THEME_VARIABLES_CSS_PATH, THEME_VARIABLES_CSS),
+        (THEME_CHROME_CSS_PATH, THEME_CHROME_CSS),
         (THEME_FONTS_CSS_PATH, THEME_FONTS_CSS),
         (THEME_BOOK_JS_PATH, THEME_BOOK_JS),
         (THEME_INDEX_HBS_PATH, THEME_INDEX_HBS),
+        (THEME_HIGHLIGHT_CSS_PATH, highlight_theme),
     ];
 
     for (subpath, contents) in files {
@@ -95,6 +113,7 @@ fn init_theme_files(theme_dir: &Utf8Path) -> Result<()> {
         LocalAsset::create_dir_all(theme_dir.as_str())?;
         LocalAsset::write_new_all(contents, path)?;
     }
+    
 
     Ok(())
 }
